6db8f57dad4d72dac593301a0792996b48cbebfd
Justin Plaatjies <j.plaatjies08@gmail.com>
2024-07-04 20:25:50 +0200

    Login and Sign-Up Front-End Cope
    Login and Sign - Up code Idea

--- app/src/main/java/com/example/grouptest/MainActivity.kt
+++ app/src/main/java/com/example/grouptest/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.grouptest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickMe: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickMe = findViewById(R.id.bntClickMe)
 
-
-
+        clickMe.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        }
     }
 }
